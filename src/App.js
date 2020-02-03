9eea09e04864ccb7de5d4f303806c2eecb79e241
Jgayle <Jgayle99@gmail.com>
2020-02-03 12:37:48 -0500

    updating app with a shop page, collection preview component and collection item

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component.jsx';
 /*
     <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link>
     <Link to='/topics/abc'>Topicsabc</Link>
@@ -41,16 +41,7 @@ const TopicDetail = props => {
             <Route path='/abc/topics/:topicId' component={TopicDetail} />
 */
 
-const HatsPage = props => {
-  return (
-  <div>
-    <Link to={`${props.match.url}13`}>TO TOPIC 13</Link>
-    <Link to={`${props.match.url}14`}>TO TOPIC 14</Link>
-    <Link to={`${props.match.url}15`}>TO TOPIC 15</Link>
-    <h1>TOPIC LIST PAGE</h1>
-  </div>
-  );
-  };
+
 
 
 function App() {
@@ -59,7 +50,7 @@ function App() {
       <BrowserRouter>
         <Switch>
             <Route exact path='/' component={HomePage} />
-            <Route exact path='/hats' component={HatsPage} />
+            <Route exact path='/shop' component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
