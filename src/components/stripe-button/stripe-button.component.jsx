e525bab1f0efeaa16cda8e661c13ad205672f451
Jgayle <Jgayle99@gmail.com>
2020-02-07 09:26:20 -0500

    Integrated Stripe payments and updated favicon.ico

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_RthDwELXP7YVZ8Miwig6HRyT00PszZw2ST';
+
+    const onToken = token =>{
+        console.log(token);
+        alert('Payment Sucessful');
+    };
+
+    return (
+        <StripeCheckout
+            label = 'Pay Now'
+            name = 'Joel Clothing Shop'
+            billingAddress
+            shippingAddress
+            image = 'https://svgshare.com/i/CUz.svg'
+            description = {`Your Total $${price}`}
+            amount = {priceForStripe}
+            panelLabel = 'Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+
+    );
+};
+
+export default StripeCheckoutButton;
